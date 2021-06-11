b2f786194a1f7bd417a4f57ecf2eeda1b63d819b
gsandvoss <gsandvoss@gmail.com>
2021-06-11 06:18:47 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by glenn sandvoss on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
