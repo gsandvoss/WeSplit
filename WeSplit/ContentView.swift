03c693ec625bdef35dc4b942f0b24deb5848ddae
gsandvoss <gsandvoss@gmail.com>
2021-06-12 07:16:58 -0700

    Two way binding

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,29 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+
     var body: some View {
+        
         Form {
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
+
+                }
             }
         }
-    }
+
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            }
         }
-    }
-}
+    
+
